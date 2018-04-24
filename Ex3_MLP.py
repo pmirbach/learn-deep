90a1d08d01f74de87d1eb3de21384f8822263144
Philip Mirbach <pmirbach@itp.uni-bremen.de>
2018-04-24 14:45:10 +0200

    Some testing with data generators.

---
+++ Ex3_MLP.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 10:09:19 2018
+
+@author: Philip
+"""
+
--- tests/test1.py
+++ tests/test1.py
@@ -7,13 +7,57 @@ Created on Mon Apr 23 15:21:11 2018
 """
 
 import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import cm
+
+from sklearn import datasets
+from sklearn import model_selection
+
+
+###correc shape for 1d data
+
+#x = np.linspace(start=0, stop=50, num=50)
+##standard 1D shape: (N,)
+#print(x.shape)
+#
+##1D data in correct shape: (N,1)
+#x_new = x.reshape((-1,1))
+#print(x_new.shape)
+
+
+###data sets 
+#boston = datasets.load_boston()
+#print(boston.DESCR)
+
+
+#X, y = datasets.make_blobs(n_samples=1000, centers=3, n_features=2)
+X, y = datasets.make_classification(n_samples=1000, n_features=2, 
+                                    n_informative=2, n_redundant=0,
+                                    n_clusters_per_class=1, n_classes=3)
+
+y_unique = np.unique(y)
+colors = cm.rainbow(np.linspace(0.0, 1.0, y_unique.size))
+
+fig1, ax = plt.subplots()
+for (this_y, color) in zip(y_unique, colors):
+    this_X = X[y == this_y]
+    ax.scatter(this_X[:,0], this_X[:,1], c=color)
+
+#model_selection.train_test_split()
+
+#exit()
+
+
+plt.show()
+
+#model_selection.train_test_split()
 
 
 
-x = np.linspace(start=0, stop=50, num=50)
-#standard 1D shape: (N,)
-print(x.shape)
 
-#1D data in correct shape: (N,1)
-x_new = x.reshape((-1,1))
-print(x_new.shape)
+
+
+
+
+
+
