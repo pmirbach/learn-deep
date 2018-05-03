917be8b875fddf46ab6ea3e4e3a57c6a3c7ebb25
Philip Mirbach <philip.mirbach@gmail.com>
2018-05-03 17:59:10 +0200

    Some averaging, experiments with number of hidden layers.

--- Ex3_MLP.py
+++ Ex3_MLP.py
@@ -13,6 +13,10 @@ from sklearn import model_selection
 from sklearn import neural_network
 
 
+#statistical evaluation
+flg_stat = 1
+
+
 
 n_layers = 4
 
@@ -22,33 +26,55 @@ X, y = datasets.make_classification(n_samples=5000, n_features=10, n_informative
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.1)
 
-clf_NN = neural_network.MLPClassifier(hidden_layer_sizes=(100,)*n_layers, activation='logistic', max_iter=500)
-
-clf_NN.fit(X_train, y_train)
-y_predict = clf_NN.predict(X_test)
 
 
-NN_shape = [coef.shape for coef in clf_NN.coefs_]
+res = np.zeros((10,6))
 
-print(NN_shape)
-print(np.sum(np.abs(y_test - y_predict)) / y_test.size)
+out_str = 'Percentage of false predictions - {} hidden layers'
+
+for j in range(6):
+    for i in range(10):
+        clf_NN = neural_network.MLPClassifier(hidden_layer_sizes=(70,)*j, activation='logistic', max_iter=500)
+        clf_NN.fit(X_train, y_train)
+        y_predict = clf_NN.predict(X_test)
+        res[i,j] = np.sum(np.abs(y_test - y_predict)) / y_test.size * 100
+        
+    print(out_str.format(j+1))
+    print(np.mean(res[:,j]))
+    
+    #    print(np.sum(np.abs(y_test - y_predict)) / y_test.size)
+
+#print(res)
 
 
-fig, axes = plt.subplots(n_layers,1)
-ims = []
 
-for i in range(n_layers):
-    im = axes[i].imshow(clf_NN.coefs_[i])
-#    fig.colorbar(im)
-    ims.append(im)
+#clf_NN = neural_network.MLPClassifier(hidden_layer_sizes=(100,)*n_layers, activation='logistic', max_iter=500)
+#
+#clf_NN.fit(X_train, y_train)
+#y_predict = clf_NN.predict(X_test)
 
-#print(len(axes))
+
+#NN_shape = [coef.shape for coef in clf_NN.coefs_]
+
+#print(NN_shape)
+#print(np.sum(np.abs(y_test - y_predict)) / y_test.size)
+
+
+#fig, axes = plt.subplots(n_layers,1)
+#ims = []
+#
+#for i in range(n_layers):
+#    im = axes[i].imshow(clf_NN.coefs_[i])
+##    fig.colorbar(im)
+#    ims.append(im)
+#
+##print(len(axes))
 
 #
 #im = ax.imshow(clf_NN.coefs_[0])
 #cb = fig.colorbar(ims[0])
 #
-plt.show()
+#plt.show()
 
 
 
