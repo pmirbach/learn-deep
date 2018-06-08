f5cf249feb59330775765a4b25b3efdd8675f4a9
Philip Mirbach <pmirbach@itp.uni-bremen.de>
2018-06-08 09:38:11 +0200

    Tflearn: Convolutional neural network.

---
+++ Ex7_ConvNN_tflearn.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  8 08:59:55 2018
+
+@author: Philip
+"""
+
+
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.normalization import batch_normalization
+
+from tflearn.datasets import mnist
+
+
+import tensorflow
+tensorflow.reset_default_graph()
+
+
+
+def load_mnist(one_hot=True):
+    X_train, y_train, X_test, y_test = mnist.load_data(one_hot=one_hot)
+    X_train = X_train.reshape([-1, 28, 28, 1])
+    X_test = X_test.reshape([-1, 28, 28, 1])
+    return X_train, y_train, X_test, y_test
+
+
+
+    
+# build network
+network = input_data(shape=[None, 28, 28, 1])
+network = conv_2d(network, nb_filter=32, filter_size=3, activation='relu', regularizer='L2')
+network = max_pool_2d(network, kernel_size=2)
+network = batch_normalization(network)
+
+network = conv_2d(network, nb_filter=64, filter_size=3, activation='relu', regularizer='L2')
+network = max_pool_2d(network, kernel_size=2)
+network = batch_normalization(network)
+
+network = fully_connected(network, n_units=128, activation='tanh', regularizer='L2', weight_decay=0.001)
+network = dropout(network, 0.7)
+
+network = fully_connected(network, n_units=256, activation='tanh', regularizer='L2', weight_decay=0.001)
+network = dropout(network, 0.7)
+
+network = fully_connected(network, n_units=10, activation='softmax')
+    
+    
+
+# network optimization
+sgd = tflearn.SGD(learning_rate=0.1, lr_decay=0.96, decay_step=1000)
+top_k = tflearn.metrics.Top_k(3)
+network = tflearn.regression(network, optimizer=sgd, metric=top_k, loss='categorical_crossentropy')
+
+
+
+
+X_train, y_train, X_test, y_test = load_mnist(one_hot=True)
+
+
+
+# train MLP
+model = tflearn.DNN(network, tensorboard_verbose=0)
+model.fit(X_train, y_train, n_epoch=20, validation_set=(X_test, y_test),
+          show_metric=True, run_id="dense_model")
+
+# predict
+y_pred = model.predict(X_test)
+
+
+
+
